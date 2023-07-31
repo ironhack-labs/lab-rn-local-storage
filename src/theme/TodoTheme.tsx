8310d4c0524416bd64e68ba2fdd314b57c650d85
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-07-31 11:19:36 -0600

    chore(*): iteration 1: project setup
    In this iteration the project is initialized

---
+++ src/theme/TodoTheme.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+export const s = StyleSheet.create({
+  app: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+  },
+  body: {
+    flex: 5,
+  },
+  footer: {
+    height: 80,
+  }
+})
