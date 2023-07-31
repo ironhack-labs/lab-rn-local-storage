5f59c234e474783a7dbcad1e111bbae1cc3fc9cf
José Pablo Gastelum Muñoz <jose.gastelum@digitalfemsa.com>
2023-07-31 11:07:20 -0700

    Add new task button component

--- src/screens/TasksListScreen.tsx
+++ src/screens/TasksListScreen.tsx
@@ -1,12 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {useTasksContext} from '../hooks';
+import {AddTaskButton} from '../components';
 
 const TasksListScreen = () => {
   const {tasks} = useTasksContext();
   return (
     <View>
       <Text>{JSON.stringify(tasks, null, 4)}</Text>
+      <AddTaskButton />
     </View>
   );
 };
