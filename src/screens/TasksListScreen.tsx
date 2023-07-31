0567782b3bc167832eb86b92249e08a0a7e989e2
José Pablo Gastelum Muñoz <jose.gastelum@digitalfemsa.com>
2023-07-31 15:52:24 -0700

    Tasks lists additional styles

--- src/screens/TasksListScreen.tsx
+++ src/screens/TasksListScreen.tsx
@@ -1,17 +1,21 @@
 import {TaskListItem} from '../components';
 import {useAppNavigation, useTasksContext} from '../hooks';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
 import Button from '../components/Button';
 import React from 'react';
+import {taskListStyles} from '../theme/TaskList.styles';
 
 const TasksListScreen = () => {
   const {tasks} = useTasksContext();
   const {navigate} = useAppNavigation();
 
   return (
-    <View style={{flex: 1}}>
+    <View style={taskListStyles.container}>
+      <Text style={taskListStyles.title}>Tasks List</Text>
       <FlatList
         data={tasks}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={({id}) => id.toString()}
         renderItem={({item}) => <TaskListItem task={item} />}
       />
       <Button
