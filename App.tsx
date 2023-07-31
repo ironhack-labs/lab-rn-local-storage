cce618c249baf7dc2dcf042921097c6dbef80888
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-07-31 11:18:35 -0600

    feat: add ui-kitten components

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+
 import {AppNavigator} from './src/navigation';
 import {AppProvider} from './src/context';
 
@@ -9,9 +13,12 @@ const App = () => (
   // NOTE: I need full screen its easier to use style inline, so the next comment remove the eslint warning
   // eslint-disable-next-line react-native/no-inline-styles
   <SafeAreaView style={{flex: 1}}>
-    <AppProvider>
-      <AppNavigator />
-    </AppProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <IconRegistry icons={EvaIconsPack} />
+      <AppProvider>
+        <AppNavigator />
+      </AppProvider>
+    </ApplicationProvider>
   </SafeAreaView>
 );
 
