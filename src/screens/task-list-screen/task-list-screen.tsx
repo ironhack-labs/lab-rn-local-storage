4e595eaf8a3cd1591bb3c22eee0dab1bc2948194
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-07-31 12:23:37 -0600

    style: add some styles to task-list screen

--- src/screens/task-list-screen/task-list-screen.tsx
+++ src/screens/task-list-screen/task-list-screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {Button} from '@ui-kitten/components';
+import {View} from 'react-native';
+import {Button, List, ListItem, Icon, Divider} from '@ui-kitten/components';
 
 import type {TaskListScreenProps} from '../../navigation/app-navigator.types';
 import {useAppCtx} from '../../context';
@@ -9,10 +9,28 @@ export const TaskListScreen = ({navigation}: TaskListScreenProps) => {
   const {tasks} = useAppCtx();
 
   return (
-    <View>
-      {tasks.map(({id, title}) => (
-        <Text key={id}>{title}</Text>
-      ))}
+    <View style={{flex: 1}}>
+      <List
+        data={tasks}
+        ItemSeparatorComponent={Divider}
+        renderItem={({item: task}) => (
+          <ListItem
+            title={task.title}
+            description={task.description}
+            // eslint-disable-next-line react/no-unstable-nested-components
+            accessoryLeft={props => (
+              <Icon
+                {...props}
+                name={
+                  task.completion
+                    ? 'checkmark-square-2-outline'
+                    : 'square-outline'
+                }
+              />
+            )}
+          />
+        )}
+      />
 
       <Button onPress={() => navigation.navigate('TaskCreation')}>
         Add Task
