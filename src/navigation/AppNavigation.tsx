dde2240456c7e9a0c120de59989da39db4d3b767
Geyser Ramírez <geyser.grs@gmail.com>
2023-07-31 11:40:47 -0600

    se agregó una librería faltante

---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,12 @@
+import {createStackNavigator} from '@react-navigation/stack';
+
+// Create a stack navigator
+const Stack = createStackNavigator();
+
+export default function AppNavitagor() {
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+    </Stack.Navigator>
+  );
+}
