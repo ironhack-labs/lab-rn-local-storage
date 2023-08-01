67f0bdc53ce105d0465cc1a72f5e4d9f4c1e60b0
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-07-31 22:18:44 -0700

    Adding styles for TaskDetails screen

--- src/screens/TaskDetailsScreen.tsx
+++ src/screens/TaskDetailsScreen.tsx
@@ -1,10 +1,13 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 import {TaskT, useTask} from '../context/taskContext';
 import CheckBox from '@react-native-community/checkbox';
 import {useState} from 'react';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackNavigatonT} from '../../navigation/AppNavigator';
+import {Button} from '../components/Button';
+import appStyles from '../styles';
+import styles from '../theme/TaskDetailsScreen.styles';
 
 type RouteParams = {
   params: {task: TaskT};
@@ -29,12 +32,24 @@ export const TaskDetailsScreen = () => {
   };
 
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{category}</Text>
-      <Text>{description}</Text>
-      <CheckBox value={completed} onValueChange={handleChangeCompleted} />
-      <Button title="Remove task" onPress={handleRemove} />
+    <View style={[appStyles.container, styles.container]}>
+      <View>
+        <Text style={styles.header}>Title</Text>
+        <Text style={styles.value}>{title}</Text>
+      </View>
+      <View>
+        <Text style={styles.header}>Category</Text>
+        <Text style={styles.value}>{category}</Text>
+      </View>
+      <View>
+        <Text style={styles.header}>Description</Text>
+        <Text style={styles.value}>{description}</Text>
+      </View>
+      <View style={styles.completedContainer}>
+        <CheckBox value={completed} onValueChange={handleChangeCompleted} />
+        <Text style={styles.value}>Completed</Text>
+      </View>
+      <Button text="Remove task" onPress={handleRemove} />
     </View>
   );
 };
